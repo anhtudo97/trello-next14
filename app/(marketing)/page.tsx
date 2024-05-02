69071d2e58b188b600d33a958fd428235393803b
anhtudo97 <anhtudo97@gmail.com>
2024-05-03 02:23:19 +0700

    feat: marketing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button';
+import { cn, headingFont, textFont } from '@/lib/utils';
+import { Medal } from 'lucide-react';
+import Link from 'next/link';
+
+// option local font
+// const headingFont = localFont({
+//     src: "../../public/fonts/font.otf"
+// });
+
+const MarketingPage = () => {
+    return (
+        <div className="flex items-center justify-center flex-col">
+            <div className={cn("flex items-center justify-center flex-col",
+                headingFont.className
+            )}>
+                <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+                    <Medal className="size-6 mr-2" /> No 1 task management
+                </div>
+
+                <h1 className="text-3xl md:text-6xl text-center text-neutral-600 mb-6 font-semibold">
+                    Taskify helps team move
+                </h1>
+                <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+                    work forward
+                </div>
+            </div>
+            <div className={
+                cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+                    textFont.className
+                )
+            }>
+                Collaborate, manage projects, and reach new productivity peak. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify
+            </div>
+
+            <Button className="mt-6" size="lg" asChild>
+                <Link href="/sign-up">
+                    Get Taskify for free
+                </Link>
+            </Button>
+        </div>
+    );
+};
+
+export default MarketingPage;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,33 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { Chakra_Petch, Poppins } from "next/font/google";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+
+export const headingFont = Chakra_Petch({
+  subsets: ["latin"],
+  weight: [
+    "400",
+    "500",
+    "600",
+    "700"
+  ]
+});
+
+export const textFont = Poppins({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ]
+});
