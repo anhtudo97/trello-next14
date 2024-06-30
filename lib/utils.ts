5e2e7f0a59fe076269dc7c331933fbb8eacf72a3
anhtudo97 <anhtudo97@gmail.com>
2024-06-30 23:07:01 +0700

    feat: update config

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,20 +1,15 @@
 import { type ClassValue, clsx } from "clsx";
 import { Chakra_Petch, Poppins } from "next/font/google";
 import { twMerge } from "tailwind-merge";
+import localFont from "next/font/local";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 
-export const headingFont = Chakra_Petch({
-  subsets: ["latin"],
-  weight: [
-    "400",
-    "500",
-    "600",
-    "700"
-  ]
+export const headingFont = localFont({
+  src: "../../public/fonts/font.woff2"
 });
 
 export const textFont = Poppins({
