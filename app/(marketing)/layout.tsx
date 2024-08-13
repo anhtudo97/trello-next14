5e1e4b7cbc0049a45d2361cd4adc89aad7eff0d3
anhtudo97 <anhtudo97@gmail.com>
2024-08-13 23:11:54 +0700

    feat: update marketing

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,10 @@
-import React, { ReactNode } from 'react';
-import Navbar from './_components/navbar';
-import Footer from './_components/footer';
+import { Footer } from "./_components/footer";
+import { Navbar } from "./_components/navbar";
 
 const MarketingLayout = ({
     children
 }: {
-    children: ReactNode;
+    children: React.ReactNode;
 }) => {
     return (
         <div className="h-full bg-slate-100">
