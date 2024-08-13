c0c5746aa6fb2286f0f963555dd974fc373d70d5
anhtudo97 <anhtudo97@gmail.com>
2024-08-13 23:12:13 +0700

    chore: context

--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,5 @@
 export const siteConfig = {
     name: "Taskify",
-    description: "Collaborate, manage projects, and reach new productivity peak. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify"
-};
+    description: "Collaborate, manage projects, and reach new productivity peaks",
+  };
+  
