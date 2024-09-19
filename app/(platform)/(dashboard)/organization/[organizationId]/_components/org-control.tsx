fa927d0df1b4493f33d3c5ba5eaf2276932c5df5
anhtudo97 <anhtudo97@gmail.com>
2024-09-20 01:59:45 +0700

    feat: dashboard

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -1,26 +1,20 @@
 "use client";
 
-import { useOrganizationList } from '@clerk/nextjs';
-import { useParams } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useOrganizationList } from "@clerk/nextjs";
 
-
-const OrgControl = () => {
-
+export const OrgControl = () => {
     const params = useParams();
     const { setActive } = useOrganizationList();
 
     useEffect(() => {
-
         if (!setActive) return;
 
         setActive({
-            organization: params.organizatioId as string
+            organization: params.organizationId as string,
         });
     }, [setActive, params.organizationId]);
 
-
     return null;
 };
-
-export default OrgControl;
