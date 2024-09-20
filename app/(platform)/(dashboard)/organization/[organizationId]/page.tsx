0e1e4abe281b8b97c97e8445650b13f92e82f7a9
anhtudo97 <anhtudo97@gmail.com>
2024-09-20 23:43:12 +0700

    feat: update package

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,21 +1,22 @@
-import { db } from '@/lib/db';
-import Form from './form';
+import { Suspense } from "react";
+
+import { Separator } from "@/components/ui/separator";
+
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
+import { checkSubscription } from "@/lib/subscription";
 
 const OrganizationIdPage = async () => {
-    const boards = await db.board.findMany();
+    const isPro = await checkSubscription();
 
     return (
-        <div className="flex flex-col space-y-4">
-            <Form  />
-
-            <div className="space-y-2">
-                {
-                    boards.map(board => (
-                        <div key={board.id}>
-                            Board title: {board.title}
-                        </div>
-                    ))
-                }
+        <div className="w-full mb-20">
+            <Info isPro={isPro} />
+            <Separator className="my-4" />
+            <div className="px-2 md:px-4">
+                <Suspense fallback={<BoardList.Skeleton />}>
+                    <BoardList />
+                </Suspense>
             </div>
         </div>
     );
